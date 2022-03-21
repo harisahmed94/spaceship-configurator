065806cedcc57d0e4419aa8d587df467760b53e1
Haris Ahmed <harisahmed94@gmail.com>
2022-03-21 07:29:27 +0200

    Removed comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Header from "./components/Header";
-import Body from "./components/Main";
+import Main from "./components/Main";
 import "./App.scss";
 
 function App() {
   return (
     <div className="container">
       <Header />
-      <Body />
+      <Main />
     </div>
   );
 }
